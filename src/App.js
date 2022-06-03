c1d76d7f4b981da167e4713bd4659b59aca76770
Unhynged <unhynged@protonmail.com>
2022-06-03 15:29:35 +1000

    Updated App.js with component Info and AddItem

--- src/App.js
+++ src/App.js
@@ -3,8 +3,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Bare bones app.</p>
+      <Info />
+      <AddItem />
+      <AddItem />
+      <AddItem></AddItem>
     </div>
+  );
+}
+
+function Info() {
+  return (
+    <div>
+      <h1>Inventory System</h1>
+      <p>Manage your stuff.</p>
+    </div>
+  );
+}
+
+function AddItem() {
+  return (
+    <form>
+      <label for="text-form">Type something: </label>
+      <input type="text" id="text-form"/>
+      <Info />
+    </form>
   );
 }
 
