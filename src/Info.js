fc6c4ec13e1a969c28a96865d3eaf5a390cd4c1a
Unhynged <unhynged@protonmail.com>
2022-06-06 09:57:36 +1000

    Updated Info.js to Class-based component
    Altered the code inside of Info.js to make a class-based component out of the original Info functional component. Utilized import React from react; and class Info extends React.Component; also required using a render method

--- src/Info.js
+++ src/Info.js
@@ -1,7 +1,11 @@
-export default function Info() {
+import React from "react"; // this makes and the class Info helps make a class based component. We use class Info and extend the React.Component. Since we're extending react we need to say import React from "react". This is the default export and so that means we can import it this way, if it wasn't, we would need to surround it in our { curly braces }.
+
+class Info extends React.Component {
+  // we then define a render method and inside the method we write what we want to be returned to render on the screen. By default the return statement from a functional component is just this render method, whereas when you create a class, you need to define manually the render method.
+  render() {
     const title = "This is my title.";
     const showTitle = true;
-  
+
     if (showTitle) {
       return (
         <div>
@@ -13,3 +17,8 @@ export default function Info() {
       return <p>empty</p>;
     }
   }
+}
+
+export default Info;
+
+// This has made a class-based component. You can see that it works the exact same as a functional component, except we have to write more. We say class info, extends react.component, we have to define a render method and then it's just a little bit different syntax. That's really the main difference between a functional and class-based component, A little bit of a different syntax. If you're getting into professional react development, then you can make the argument, which is better. React at the moment is recommending using functional components. Unless you have a ton of state in which they are saying you should use class-based component.
