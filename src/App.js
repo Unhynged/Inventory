94e256b646ebe621363c1a83ba5f388d38825e29
Unhynged <unhynged@protonmail.com>
2022-06-06 12:28:49 +1000

    Modified App.js and Info.js
    Made modifications to props as well as utilizing state.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 }
 
 function ButtonState() {
-  const [title, setTitle] = useState(""); // You could name these whatever you want but it's good practice to have set followed by the name of the state variable you're going to be updating. 
+  const [title, setTitle] = useState(""); 
   const [count, setCount] = useState(0);
 
   const updateTitleClicked = () => {
@@ -24,19 +24,23 @@ function ButtonState() {
     setCount(count + 1);
   }
 
-  return ( // This component <--- v
+  return ( // title and count in "Data" are both state, they are then passed in the props to this component and what happens is whenever you have the state updating it's rerendering this entire thing. The props here are going to change based on what the state is and that means we're going to show that in the components, it's going to have those updated values. 
     <div>
-      <p>Title: {title}</p>
-      <p>Counter: {count}</p>
+      <Data title={title} count={count} /> 
       <button onClick={updateTitleClicked}>Update Title</button> 
       <button onClick={updateCounterClicked}>Update Counter</button>
     </div>
-  ); // This component <--- ^
-} // Whenever you want to call a JavaScript function, when a button is clicked is you need to use onClick={}. What you need to do is pass a function that you want to be called when this button is clicked. So in this case, the function that I want to be called is setTitle and setCount but these functions need to be called with arguments and those arguments are going to be what I want to set the title to or set the count to. 
+  ); 
+} 
 
-// We want to have some state, this state is going to pertain to this title and this counter and when we press these buttons we want to update the state, we want to change the state. In React, we use hooks. A hook is a function thats provided by react that you can use to hook into and modify it's behaviour. To do this we have to import something that allow us to use state. import { useState } from "react"; 
-// Whenever you want to setup a state in a functional component is you create a constant e.g. const [] and you define two values. First you define the actual variable thats going to store your state and then you define the name of the function you're going to use to update this state or update this variable. In this case, title then setTitle < const [title, setTitle] = useState("") > and this'll be equal to useState. inside of useState you define the default state. If I want to modify title I use the setTitle function. 
-// Whenever you update the state of a component, it tells that component to rerender itself on the screen. So only that component and any children components its rendering but not the entire application. The idea here is that inside of that button state, as soon as either of these states are modified, this entire component will rerender itself. Only the individual component is going to rerender everything that gets returned. If you have any children components inside of here, all of those will be rerendered. 
-// This gets into performance, e.g. if all of your state is being stored in say an upper level component like "App" then that means whenever that state is changed, its going to rerender the entire application, which is obviously going to take longer than just re rendering one component at a time. 
+function Data(props) { // This will take in some props. Remember, if we want to pass information down to components from one of the upper level components we can pass that through the props. 
+  return (
+    <div>
+      <p>Title: {props.title}</p>
+      <p>Count: {props.count}</p>
+    </div>)
+}
 
 export default App;
+
+// What happens when we want to pass this state down to components that are different components. 
--- src/Info.js
+++ src/Info.js
@@ -1,37 +1,34 @@
-import React from "react"; // this makes and the class Info helps make a class based component. We use class Info and extend the React.Component. Since we're extending react we need to say import React from "react". This is the default export and so that means we can import it this way, if it wasn't, we would need to surround it in our { curly braces }.
+import React from "react";
 import { PropTypes } from "prop-types";
 
 class Info extends React.Component {
-  // we then define a render method and inside the method we write what we want to be returned to render on the screen. By default the return statement from a functional component is just this render method, whereas when you create a class, you need to define manually the render method.
+  // Inside info here, going to start by defining what I want held in my state.
+
   constructor(props) {
-      super(props); // To do default props for our class-based components, involves defining a constructor(props) { super(props);} It's very important that you call this super constructor here. What this is doing is calling the constructor of the class that you are inheriting from, in this case, react.component ; the reason you need to do this is because React.Component has it's own constructor. That constructor is going to set up the actual component and need to manually call it when you go ahead and override the constructor inside the of the child class and you need to make sure you pass through the same props that are accepted in the constructor(props) to the super(props) so that it will do everything properly. Inside of the class based component, it's recommended you do not decompose your props. 
-      console.log(props);
+    super(props);
+    this.state = {
+      // make this equal to a JavaScript object. Inside you define your keys, your state values and then what their default values are going to be.
+      count: 0,
+    }; // this is the only thing that we're going to be storing in our state and the reason we know that is because we defined that in the constructor.
+    // this.buttonPressed = this.buttonPressed.bind(this);
   }
-  
+
+  buttonPressed() {
+    this.setState({ // any of the fields that you pass in here are kind of merged with the existing state. Thus if you just pass count, it will just update count. Any of the other state values will stay the same.
+        count: this.state.count + 1
+    });
+  } // What happens by default when I define this function here inside of the class is it does not have access to the "this" keyword. To give it access to the "this" keyword you need to actually bind it into this class because right now this is just a function as defined insidef of this class. Doesn't mean it's actually a part of this kind of class instance, and so it can't access this. Two ways to fix this. First way is to define an arrow function inside <button onClick={() => this.buttonPressed()}> . The alternative is to go up to the constructor and you can say this.buttonPressed = this.buttonPressed.bind(this);
+
   render() {
-    const title = this.props.title; // Whenever you are not inside of the constructor and you want to access the props you need to use the "this" keyword. These work the exact same as they do in the functional components, they are read only, you cannot modify them. 
-    const showTitle = true;
-
-    if (showTitle) {
-      return (
-        <div>
-          <h1>{title}</h1>
-          <p>Manage your stuff.</p>
-        </div>
-      );
-    } else {
-      return <p>empty</p>;
-    }
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={() => this.buttonPressed()}>Click Me!</button>
+      </div>
+    );
   }
-}
+} // Recommended to use the arrow function, the reason why the arrow function works is because since this is a function defined inside of the render method which is already attached to this class, it has access to this keyword.
 
-Info.defaultProps = {
-    title: "Default",
-}
-
-Info.propTypes = {
-    title: PropTypes.string,
-}
 export default Info;
 
-// This has made a class-based component. You can see that it works the exact same as a functional component, except we have to write more. We say class info, extends react.component, we have to define a render method and then it's just a little bit different syntax. That's really the main difference between a functional and class-based component, A little bit of a different syntax. If you're getting into professional react development, then you can make the argument, which is better. React at the moment is recommending using functional components. Unless you have a ton of state in which they are saying you should use class-based component.
+// Classed based component state.
