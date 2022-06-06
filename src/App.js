9fc2baafeaa2f23acd3914719ede9d2f227d4ef4
Unhynged <unhynged@protonmail.com>
2022-06-06 11:45:42 +1000

    Modified App.js
    Made modifications to App.js, removing the original AppItems functions. Replaced with functions for setting and updating title and count

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
 import './App.css';
 import Info from "./Info.js";
-import { PropTypes } from "prop-types"
+import { PropTypes } from "prop-types";
+import { useState } from "react";
 
-function App() { // Whenever you pass a number you need to put it in {} curly braces. As you can see in AddItem, we've added properties to the tag, as we can pass them to our own components. We just write the name of our property e.g. text="Tim" number={2} These are now properties of this tag. How do we access these properties? You need to go to AddItem() and define a parameter called props.
+function App() { 
   return (
     <div className="App">
       <Info/>
-      <AddItem text="Tim" number={2}/> 
-      <AddItem text="Joe" />
-      <AddItem></AddItem>
+      <ButtonState/>
     </div>
   );
 }
 
-function AddItem(props) { // Looking at the input tag here we can see what a prop might look like. We have "type", we have "value" and we have "id" and all these things change the behaviour or what the input tag looks like. We can use this props variable here to access all of the properties that are passed through and this by default is going to be a javascript object.
-//function AddItem({ text, number }) { // This is decomposing or deconstructing the javascript. This is going to look in that javascript object and assign any object or any field that's called text to this text variable and any field called number to this number variable. You'd only want to do this if you only have a few props, it can get kind of messy if you are writing out hundreds of differen properties and decomposing all of them but this is a good way to extract the ones that you want, especially if you only have a few that you're looking for.
-  const value = props.text;
-  //const value = text;
+function ButtonState() {
+  const [title, setTitle] = useState(""); // You could name these whatever you want but it's good practice to have set followed by the name of the state variable you're going to be updating. 
+  const [count, setCount] = useState(0);
 
-  return (
-    <form>
-      <label for="text-form">Type something: </label>
-      <input type="text" value={props.text} id="text-form"/> 
-      <p>{props.number}</p>
-    </form>
-  );
-}
+  const updateTitleClicked = () => {
+    setTitle("We now have a title!");
+  }
 
-AddItem.defaultProps = {
-  number: 2,
-  text: "default",
-};
+  const updateCounterClicked = () => {
+    setCount(count + 1);
+  }
 
-AddItem.propTypes = { // When using PropTypes need to import { PropTypes } from "prop-types" up top. 
-  number: PropTypes.number,
-  text: PropTypes.string,
-}
+  return ( // This component <--- v
+    <div>
+      <p>Title: {title}</p>
+      <p>Counter: {count}</p>
+      <button onClick={updateTitleClicked}>Update Title</button> 
+      <button onClick={updateCounterClicked}>Update Counter</button>
+    </div>
+  ); // This component <--- ^
+} // Whenever you want to call a JavaScript function, when a button is clicked is you need to use onClick={}. What you need to do is pass a function that you want to be called when this button is clicked. So in this case, the function that I want to be called is setTitle and setCount but these functions need to be called with arguments and those arguments are going to be what I want to set the title to or set the count to. 
+
+// We want to have some state, this state is going to pertain to this title and this counter and when we press these buttons we want to update the state, we want to change the state. In React, we use hooks. A hook is a function thats provided by react that you can use to hook into and modify it's behaviour. To do this we have to import something that allow us to use state. import { useState } from "react"; 
+// Whenever you want to setup a state in a functional component is you create a constant e.g. const [] and you define two values. First you define the actual variable thats going to store your state and then you define the name of the function you're going to use to update this state or update this variable. In this case, title then setTitle < const [title, setTitle] = useState("") > and this'll be equal to useState. inside of useState you define the default state. If I want to modify title I use the setTitle function. 
+// Whenever you update the state of a component, it tells that component to rerender itself on the screen. So only that component and any children components its rendering but not the entire application. The idea here is that inside of that button state, as soon as either of these states are modified, this entire component will rerender itself. Only the individual component is going to rerender everything that gets returned. If you have any children components inside of here, all of those will be rerendered. 
+// This gets into performance, e.g. if all of your state is being stored in say an upper level component like "App" then that means whenever that state is changed, its going to rerender the entire application, which is obviously going to take longer than just re rendering one component at a time. 
 
 export default App;
-
-// props are read only values. That means you cannot change a property from inside a component that it is being passed to. 
-// Sometimes you need to ensure there is a value for some prop. We can give a default value for this property so that if it is not passed we will use the default instead. E.g. function AddItem({ text, number = 4 }) { that's a way that you can do a default prop. Can also do AddItem.defaultProps = { number: 1,  };
-// If we want to enforce or at least tell our user what type we are looking for for a specific prop, we can do that. 
