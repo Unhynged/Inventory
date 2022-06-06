efa40e0fd6c651b33d7e8dbe8e5d73e1c96059ed
Unhynged <unhynged@protonmail.com>
2022-06-06 11:06:57 +1000

    Modified App.js and Info.js
    Made modifications to App.js and Info.js with regards to props

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import './App.css';
 import Info from "./Info.js";
+import { PropTypes } from "prop-types"
 
 function App() { // Whenever you pass a number you need to put it in {} curly braces. As you can see in AddItem, we've added properties to the tag, as we can pass them to our own components. We just write the name of our property e.g. text="Tim" number={2} These are now properties of this tag. How do we access these properties? You need to go to AddItem() and define a parameter called props.
   return (
     <div className="App">
-      <Info />
+      <Info/>
       <AddItem text="Tim" number={2}/> 
       <AddItem text="Joe" />
-      <AddItem text="Test"></AddItem>
+      <AddItem></AddItem>
     </div>
   );
 }
 
-//function AddItem(props) { // Looking at the input tag here we can see what a prop might look like. We have "type", we have "value" and we have "id" and all these things change the behaviour or what the input tag looks like. We can use this props variable here to access all of the properties that are passed through and this by default is going to be a javascript object.
-function AddItem({ text, number }) { // This is decomposing or deconstructing the javascript. This is going to look in that javascript object and assign any object or any field that's called text to this text variable and any field called number to this number variable. 
-  //const value = props.text;
-  const value = text;
+function AddItem(props) { // Looking at the input tag here we can see what a prop might look like. We have "type", we have "value" and we have "id" and all these things change the behaviour or what the input tag looks like. We can use this props variable here to access all of the properties that are passed through and this by default is going to be a javascript object.
+//function AddItem({ text, number }) { // This is decomposing or deconstructing the javascript. This is going to look in that javascript object and assign any object or any field that's called text to this text variable and any field called number to this number variable. You'd only want to do this if you only have a few props, it can get kind of messy if you are writing out hundreds of differen properties and decomposing all of them but this is a good way to extract the ones that you want, especially if you only have a few that you're looking for.
+  const value = props.text;
+  //const value = text;
 
   return (
     <form>
       <label for="text-form">Type something: </label>
-      <input type="text" value={value} id="text-form"/> 
-      <p>{number}</p>
+      <input type="text" value={props.text} id="text-form"/> 
+      <p>{props.number}</p>
     </form>
   );
 }
 
+AddItem.defaultProps = {
+  number: 2,
+  text: "default",
+};
+
+AddItem.propTypes = { // When using PropTypes need to import { PropTypes } from "prop-types" up top. 
+  number: PropTypes.number,
+  text: PropTypes.string,
+}
+
 export default App;
+
+// props are read only values. That means you cannot change a property from inside a component that it is being passed to. 
+// Sometimes you need to ensure there is a value for some prop. We can give a default value for this property so that if it is not passed we will use the default instead. E.g. function AddItem({ text, number = 4 }) { that's a way that you can do a default prop. Can also do AddItem.defaultProps = { number: 1,  };
+// If we want to enforce or at least tell our user what type we are looking for for a specific prop, we can do that. 
--- src/Info.js
+++ src/Info.js
@@ -1,9 +1,15 @@
 import React from "react"; // this makes and the class Info helps make a class based component. We use class Info and extend the React.Component. Since we're extending react we need to say import React from "react". This is the default export and so that means we can import it this way, if it wasn't, we would need to surround it in our { curly braces }.
+import { PropTypes } from "prop-types";
 
 class Info extends React.Component {
   // we then define a render method and inside the method we write what we want to be returned to render on the screen. By default the return statement from a functional component is just this render method, whereas when you create a class, you need to define manually the render method.
+  constructor(props) {
+      super(props); // To do default props for our class-based components, involves defining a constructor(props) { super(props);} It's very important that you call this super constructor here. What this is doing is calling the constructor of the class that you are inheriting from, in this case, react.component ; the reason you need to do this is because React.Component has it's own constructor. That constructor is going to set up the actual component and need to manually call it when you go ahead and override the constructor inside the of the child class and you need to make sure you pass through the same props that are accepted in the constructor(props) to the super(props) so that it will do everything properly. Inside of the class based component, it's recommended you do not decompose your props. 
+      console.log(props);
+  }
+  
   render() {
-    const title = "This is my title.";
+    const title = this.props.title; // Whenever you are not inside of the constructor and you want to access the props you need to use the "this" keyword. These work the exact same as they do in the functional components, they are read only, you cannot modify them. 
     const showTitle = true;
 
     if (showTitle) {
@@ -19,6 +25,13 @@ class Info extends React.Component {
   }
 }
 
+Info.defaultProps = {
+    title: "Default",
+}
+
+Info.propTypes = {
+    title: PropTypes.string,
+}
 export default Info;
 
 // This has made a class-based component. You can see that it works the exact same as a functional component, except we have to write more. We say class info, extends react.component, we have to define a render method and then it's just a little bit different syntax. That's really the main difference between a functional and class-based component, A little bit of a different syntax. If you're getting into professional react development, then you can make the argument, which is better. React at the moment is recommending using functional components. Unless you have a ton of state in which they are saying you should use class-based component.
